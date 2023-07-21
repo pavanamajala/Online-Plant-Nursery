89d626e805e1443dd4d56fb6f9a196995ac2a74a
sxa141101 <sxa141101@darden.com>
2023-07-21 19:20:56 +0530

    home page added

--- plantnursery/src/App.js
+++ plantnursery/src/App.js
@@ -1,5 +1,6 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home/Home";
 import CreateEditSeed from "./seed/CreateEditSeed";
 import ViewSeed from './seed/ViewSeed';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/' Component={Home}/>
           <Route path='/seed/view' Component={ViewSeed}/>
           <Route path='/seed/create' Component={CreateEditSeed}/>
           <Route path='/seed/edit/:seedId' Component={CreateEditSeed}/>
