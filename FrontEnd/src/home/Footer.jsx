ec6072b91a1f10f3d2fc194b35514f3a9e4689b1
Pavan Amajala <pavanamajala@gmail.com>
2023-07-29 11:00:09 +0530

    new changes added

--- FrontEnd/src/home/Footer.jsx
+++ FrontEnd/src/home/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
       <div style={{ position:'fixed', bottom:'0', width: '100%', backgroundColor: 'black', color: 'white', display: 'flex', height: '25px', textAlign: 'center'}}>
-          <div style={{ color: 'white'}}>@author pavan_amajala</div>
+          <div style={{ color: 'white'}}>@pavan_amajala</div>
           <div style={{position: 'absolute', right: '0', width: '54% ', display: 'flex', justifyContent: 'space-between'}}>
               <a href='https://www.linkedin.com/in/sri-ram-pavan-amajala-a31404192/' target="_blank" style={{color:'#0077B5', textDecoration: 'none'}}>LinkedIn</a>
               <a href='https://www.instagram.com/pavan.in' target="_blank" style={{color:'#E4405F', textDecoration: 'none'}}>Instagram</a>
--- FrontEnd/src/view/ViewSites.jsx
+++ FrontEnd/src/view/ViewSites.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './style.css'
+import bg from '../images/background.jpg'
 
 export const ViewSites = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center'}}>
+        <img src={bg} alt='img' style={{position: 'absolute', top: '63px', opacity: '0.4', height: '90%', width: '100%'}}></img>
         <div style={{textAlign: 'center', fontWeight: 'bold', fontFamily: 'inherit'}}>     
         <div className= "blinking-text">Coming Soon !!!!!!!</div>
-           <div style={{marginTop: '50%', color: 'darkred'}}>
+           <div style={{marginTop: '50%'}}>
            Thanks for Coming <br/>
             A brand new website is coming here<br/>
             Please wait a little more <br/>
-            Wanna know me more Just click on the links below
+            Wanna know me more? Just click on the links below
            </div>
         </div>
     </div>
